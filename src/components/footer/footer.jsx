e02ac3cbb05c28ccd72ae9a25ca32d358b8061f6
kokinz <nes-t@mail.ru>
2021-11-08 00:36:49 +0300

    init

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+
+  return (
+    <>
+      Hello
+    </>
+  );
+}
+
+export default Footer;
---
+++ src/components/header/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+
+  return (
+    <>
+      Hello
+    </>
+  );
+}
+
+export default Header;
