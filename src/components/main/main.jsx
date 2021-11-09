18ce5588430bf625633558ea2eb3b60e07c6fc8c
kokinz <nes-t@mail.ru>
2021-11-09 21:04:30 +0000

    breadcrumbs

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -4,7 +4,24 @@ function Main() {
 
   return (
     <main className="main">
-      hello
+      <div className="main__wrapper container">
+        <h1 className="main__header">Каталог гитар</h1>
+
+        <nav className="main__nav">
+          <ul className="main__breadcrumbs list">
+            <li>
+              <a href="/#" className="main__breadcrumb link">
+                Главная
+              </a>
+            </li>
+            <li>
+              <a href="/#" className="main__breadcrumb link">
+                Каталог
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </main>
   );
 }
