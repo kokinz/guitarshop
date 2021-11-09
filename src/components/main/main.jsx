ba7d73a960b62688c71f4a21003d254f5730123a
kokinz <nes-t@mail.ru>
2021-11-09 18:28:50 +0000

    main background

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,12 +1,11 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 
 function Main() {
 
   return (
-    <p>
-      Hello
-    </p>
+    <main className="main">
+      hello
+    </main>
   );
 }
 
